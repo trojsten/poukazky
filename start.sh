0468c244bdce0f3c1c8c037f7db337d689f3511a
Aiq0 <66842415+Aiq0@users.noreply.github.com>
2026-06-10 21:12:46 +0200

    fix: increase gunicorn timeout
    typst template takes a while to render

--- start.sh
+++ start.sh
@@ -10,5 +10,5 @@ python manage.py migrate
 if [ "$env" = "dev" ]; then
   exec python manage.py runserver 0.0.0.0:8000
 else
-  exec /base/gunicorn.sh poukazky.wsgi
+  exec /base/gunicorn.sh --timeout 90 poukazky.wsgi
 fi
